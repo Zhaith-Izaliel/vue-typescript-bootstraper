15e9791cb03080f489694516c879318a66dd78b1
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-01-18 10:37:13 +0100

    chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,78 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:jsdoc/recommended-typescript-error",
+    "airbnb-base",
+    "airbnb-typescript/base",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint", "jsdoc", "@stylistic/eslint-plugin-js"],
+  rules: {
+    "no-constant-binary-expression": "warn",
+    "@stylistic/js/eol-last": ["error", "always"],
+    "@stylistic/js/brace-style": ["error", "1tbs", { allowSingleLine: true }],
+    "@stylistic/js/array-element-newline": [
+      "error",
+      { multiline: true, minItems: 3 },
+    ],
+    "@stylistic/js/array-bracket-spacing": ["error", "always"],
+    "no-constructor-return": "error",
+    "no-duplicate-imports": "error",
+    "no-template-curly-in-string": "error",
+    "no-self-compare": "warn",
+    "require-atomic-updates": "error",
+    "default-case": "error",
+    curly: "error",
+    "max-depth": ["error", 3],
+    "dot-notation": "error",
+    "no-console":
+      process.env.NODE_ENV === "production"
+        ? ["error", { allow: ["warn", "error"] }]
+        : "off",
+    "new-cap": "error",
+    "no-else-return": ["error", { allowElseIf: false }],
+    "sort-imports": [
+      "warn",
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+        allowSeparatedGroups: false,
+      },
+    ],
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "variable",
+        format: ["camelCase", "PascalCase", "UPPER_CASE"],
+      },
+      {
+        selector: "function",
+        format: ["camelCase", "PascalCase"],
+      },
+      {
+        selector: "typeLike",
+        format: ["PascalCase"],
+      },
+    ],
+    eqeqeq: "error",
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+  },
+};
+
---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = inputs: with inputs;
+  flake-utils.lib.eachDefaultSystem (system:
+    with import nixpkgs { inherit system; };
+    let
+      nodejs-package = pkgs.nodejs_21;
+    in
+    rec {
+      workspaceShell = pkgs.mkShell {
+        # nativeBuildInputs is usually what you want -- tools you need to run
+        nativeBuildInputs = with pkgs; [
+          nodejs-package
+          node2nix
+          gnumake
+          nodePackages.typescript-language-server
+        ];
+      };
+
+      devShells = {
+        # nix develop
+        "${system}".default = workspaceShell;
+        default = workspaceShell;
+      };
+      packages = {
+        default = (import ./nix {
+          inherit pkgs system;
+          nodejs = nodejs-package;
+        }).package;
+        shell = devShells.default;
+      };
+    }
+  );
+}
+

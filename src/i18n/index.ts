984ba2e9e32db4f960c56f186f148ce4d34b1747
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-01-18 15:52:56 +0100

    fix: tailwindcss

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,11 +3,11 @@ import { createI18n } from 'vue-i18n';
 export default createI18n({
   locale: 'en', // set locale
   fallbackLocale: 'en', // set fallback locale
-  messages: {
+  messages: { // set locale messages
     en: {
       message: {
         hello: 'Hello World',
       },
     },
-  }, // set locale messages
+  },
 });
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { createApp } from 'vue';
 import router from '@/router';
 import i18n from '@/i18n';
 import App from '@/App.vue';
-import '@/styles/app/all.css';
+import '@/styles/all.css';
 
 createApp(App).use(router).use(i18n).mount('#app');

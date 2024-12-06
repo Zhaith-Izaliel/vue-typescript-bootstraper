3dc6f1e576cdec8e5c0ef2314480d205e3a6e1fa
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-12-07 00:37:41 +0100

    feat(flake): add template, update to 24.11

--- flake.nix
+++ flake.nix
@@ -2,21 +2,17 @@
   description = "An opinionated bootstraper to create a Vue.js application, with first class Typescript support, powered by Vite.js";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
   outputs = inputs @ {flake-parts, ...}: let
     inherit (packageJson) version name;
     packageJson = builtins.fromJSON (builtins.readFile ./package.json);
   in
-    flake-parts.lib.mkFlake {inherit inputs;} ({withSystem, ...}: {
+    flake-parts.lib.mkFlake {inherit inputs;} ({...}: {
       systems = ["x86_64-linux" "aarch64-darwin" "x86_64-darwin"];
 
-      perSystem = {
-        pkgs,
-        system,
-        ...
-      }: {
+      perSystem = {pkgs, ...}: {
         devShells = {
           # nix develop
           default = pkgs.mkShell {
@@ -30,6 +26,13 @@
 
         packages = {
           default = pkgs.callPackage ./nix {inherit version name;};
+        };
+      };
+
+      flake = {
+        templates.default = {
+          description = "An opinionated bootstraper to create a Vue.js application, with first class Typescript support, powered by Vite.js.";
+          path = builtins.path {path = inputs.self;};
         };
       };
     });
